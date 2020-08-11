3eb997043227b0be090bbd119d89500f2836d178
szymon-polaczy <polaczyszymon@gmail.com>
2020-08-11 15:14:47 +0200

    Added middleman for CORS

--- server/index.js
+++ server/index.js
@@ -1,33 +1,13 @@
 const express = require('express');
+const { response } = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (request, respond) => {
-
-
-
-    /*----PART THAT I HAVE NO IDEAD WHAT's GOIGN ON---*/
-
-
-    // Website you wish to allow to connect
-    respond.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
-    respond.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    respond.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    respond.setHeader('Access-Control-Allow-Credentials', true);
-
-    
-    /*----PART THAT I HAVE NO IDEAD WHAT's GOIGN ON---*/
-
-
-
-    
     respond.json({
         message: 'Meower! ^^'
     })
