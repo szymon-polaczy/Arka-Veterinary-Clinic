41377c6ceb5f7695dafcc25ce739db64b3db75cb
szymon-polaczy <polaczyszymon@gmail.com>
2020-08-12 21:08:44 +0200

    Added two more section prototypes for the HomePage

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -67,6 +67,69 @@ const WelcomeInfoSection = Styled.section`
 	}
 `
 
+const PartnersSection = Styled.section`
+	h2 {
+		text-align: center;
+	}
+
+	article {
+		display: flex;
+		align-items: center;
+		flex-flow: wrap;
+		justify-content: center;
+
+		img {
+			max-height: 5rem;
+			margin: .65rem;
+		}
+	}
+`
+
+const PeopleSection = Styled.section`
+	.wrapper {
+		max-width: 1200px;
+		margin: 0 auto;
+
+		h2 {
+			text-align: center;
+			margin-bottom: .35rem;
+		}
+
+		small {
+			text-align: center;
+			display: block;
+		}
+
+		section {
+			display: flex;
+			flex-flow: wrap;
+			align-items: center;
+			justify-content: center;
+			margin-top: 1rem;
+
+			article {
+				width: 350px;
+				margin: 1rem;
+				text-align: center;
+				line-height: 135%;
+
+				img {
+					width: 350px;
+					height: 350px;
+					border-radius: 50%;
+					overflow: hidden;
+					object-fit: cover;
+					object-position: center;
+				}
+
+				.specjalnie-dla-doktorka-na-chwile {
+					object-position: top;
+				}
+			}
+		}
+	}
+`
+
 const HomePage = () => {
 	return (
 		<HomePageContainer>
@@ -76,7 +139,7 @@ const HomePage = () => {
 			<WelcomeInfoSection>
 				<section className="img-section">
 					<h2>Arka <br/> Poznajmy się</h2>
-					<img src="Images/cat.jpg" alt="Kot"/>
+					<img src="Images/cat-welcomeinfo.jpg" alt="Kot"/>
 				</section>
 				<section className="info-section">
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
@@ -106,28 +169,67 @@ const HomePage = () => {
 				</section>
 			</WelcomeInfoSection>
 
-			<br/><br/>
-
 			<b>Co o nas mówią? [<a href="https://www.skvet.pl/">Tak jak tutaj</a>]</b> <br/>
 			Tutaj żeby wyświetlały się opinie osób które miał u nas zwierzaka - zdjęcie zwierzaka (opcjonalne) | opinia | podpis
 
-			<br/><br/>
-
-			<b>Nasi specjaliście | Nasza załoga [Tak jak <a href="https://www.skvet.pl/">tutaj</a> i <a href="http://pulsvet.com.pl/">tutaj</a>]</b><br/>
+			{/*<b>Nasi specjaliście | Nasza załoga [Tak jak <a href="https://www.skvet.pl/">tutaj</a> i <a href="http://pulsvet.com.pl/">tutaj</a>]</b><br/>
 			Chcę żeby to było schludnie jak w skvet-cie ale żeby to bardziej u nas była karuzela z pracującymi osobami / po prostu ich 
-			wszystkich wyświetlić na raz (Potem fajnie by też było dodać osoby które są ochotnikami w Łapie - tam też je dodać)
-
-			<br/><br/>
-
-			<b>Nasi partnerzy [<a href="http://www1.up.poznan.pl/ucmw/">Tak jak tutaj</a>]</b><br/>
-			Żeby się tutaj na dole prezentowały ikony naszych partnerów | Schludnie i przejrzyście
-
-			<br/><br/>
+			wszystkich wyświetlić na raz (Potem fajnie by też było dodać osoby które są ochotnikami w Łapie - tam też je dodać)*/}
+			<PeopleSection>
+				<div className="wrapper">
+					<h2>Oto nasza załoga</h2>
+					<small>, która zawsze dba aby twój miłośnik dostał to co dla niego najlepsze</small>
+					<section>
+						<article>
+							<img src="Images/people/doktorek.jpg" alt="Doktor"  className="specjalnie-dla-doktorka-na-chwile"/>
+							<h3>Technik Weterynari</h3>
+							<p>Tutaj jakiś króciótki opis czy coś, no nie wiem coś by chyba się przydało</p>
+						</article>
+						<article>
+							<img src="Images/people/iza.jpg" alt="Iza"/>
+							<h3>Technik Weterynari</h3>
+							<p>Tutaj jakiś króciótki opis czy coś, no nie wiem coś by chyba się przydało</p>
+						</article>
+						<article>
+							<img src="Images/people/michalina.jpg" alt="Michalina"/>
+							<h3>Technik Weterynari</h3>
+							<p>Tutaj jakiś króciótki opis czy coś, no nie wiem coś by chyba się przydało</p>
+						</article>
+						<article>
+							<img src="Images/people/julia.jpg" alt="Żaneta"/>
+							<h3>Technik Weterynari</h3>
+							<p>Tutaj jakiś króciótki opis czy coś, no nie wiem coś by chyba się przydało</p>
+						</article>
+						<article>
+							<img src="Images/people/krzysiu.jpg" alt="Krzysiek"/>
+							<h3>Technik Weterynari</h3>
+							<p>Tutaj jakiś króciótki opis czy coś, no nie wiem coś by chyba się przydało</p>
+						</article>
+						<article>
+							<img src="Images/people/maria.jpg" alt="Maria"/>
+							<h3>Technik Weterynari</h3>
+							<p>Tutaj jakiś króciótki opis czy coś, no nie wiem coś by chyba się przydało</p>
+						</article>
+					</section>
+				</div>
+			</PeopleSection>
 
 			<b>Mały kontakt + Mapka + Link do rezerwacji [<a href="https://www.skvet.pl/">Tak jak tutaj</a>]</b><br/>
 			Krótki kontakt z tylko najważniejszymi informacjami + zachęta do rezerwacji (strona kontakt) + mapka schludnie wyglądająca
 
-			<br/><br/>
+
+			{/*<b>Nasi partnerzy [<a href="http://www1.up.poznan.pl/ucmw/">Tak jak tutaj</a>]</b><br/>
+			Żeby się tutaj na dole prezentowały ikony naszych partnerów | Schludnie i przejrzyście*/}
+			<PartnersSection>
+				<h2>Nasi Partnerzy</h2>
+				<article>
+					<img src="Images/partners/hipra.png" alt="hipra"/>
+					<img src="Images/partners/hipromine.png" alt="hipromine"/>
+					<img src="Images/partners/kruuse.png" alt="kruuse"/>
+					<img src="Images/partners/merial.png" alt="merial"/>
+					<img src="Images/partners/royal-canin.png" alt="royal-canin"/>
+				</article>
+			</PartnersSection>
 
 			{/*<div>
 					Icons made by 
