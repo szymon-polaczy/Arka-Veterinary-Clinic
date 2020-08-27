1846951b32e1e927fff0bd02a62bfef6d2e395f4
szymon-polaczy <polaczyszymon@gmail.com>
2020-08-27 16:05:54 +0200

    It didn't fix the issue

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,9 +1,12 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+//const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = function override(config, env) {
   if (!config.devServer)
     config.devServer = {}
-    
+
+  if (!config.output.publicPath)
+    config.output.assign({publicPath: '/'})
+
   if (process.env.NODE_ENV === 'production') {
     config.devServer = {
       historyApiDallback: true,
